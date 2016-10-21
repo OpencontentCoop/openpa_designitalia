393869e748fe1df27fd7e00e0355f03367a495ac
Luca Realdi <lr@opencontent.it>
2016-10-21 14:09:56 +0200

    css fixes

--- settings/ezjscore.ini.append.php
+++ settings/ezjscore.ini.append.php
@@ -5,7 +5,8 @@ ForceScriptSettingsExtension=openpa_theme_2014
 LoadFromCDN=disabled
 ExternalScripts[jquery]=://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
 ExternalScripts[jqueryUI]=://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js
-LocalScripts[jquery]=jquery-1.10.2.min.js
-LocalScripts[jqueryUI]=jquery-ui-1.10.3.custom.min.js
+LocalScripts[jquery]=vendor/jquery.min.js
+LocalScripts[jqueryUI]=vendor/jquery-ui.min.js
+
 
 */ ?>
