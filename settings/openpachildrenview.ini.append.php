a2fc941e71ae00fe64d2d6baed78562200618ea6
Luca Realdi <lr@opencontent.it>
2017-05-11 15:17:50 +0200

    fix settings

--- settings/openpachildrenview.ini.append.php
+++ settings/openpachildrenview.ini.append.php
@@ -6,14 +6,14 @@ AvailableViews[chart]=chart
 
 [ChildrenView_advanced_datatable]
 edit_template=true
-edit_template_help=class|attribute_identifier,attribute_identifier,attribute_identifier|profondità
+edit_template_help=<class>|<attribute_identifier>,<attribute_identifier>,<attribute_identifier>,...|<depth>
 hide_menu=true
 label=Tabella
 icon_class=fa fa-table
 
 [ChildrenView_chart]
 edit_template=true
-edit_template_help=class|row attribute|column attribute|strategy (avg, total, ...)
+edit_template_help=<class>|<row attribute>|<column attribute>[-<option>]|<option>,<option>,...
 hide_menu=true
 label=Grafico
 icon_class=fa fa-bar-chart
