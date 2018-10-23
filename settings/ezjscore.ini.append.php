ffa7e01714e64e0fe6c1aa34d5304eb35cd4426d
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2018-10-23 12:00:22 +0200

    ezjscore settings update

--- settings/ezjscore.ini.append.php
+++ settings/ezjscore.ini.append.php
@@ -3,8 +3,8 @@
 [eZJSCore]
 ForceScriptSettingsExtension=openpa_designitalia
 LoadFromCDN=disabled
-ExternalScripts[jquery]=://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-ExternalScripts[jqueryUI]=://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js
+ExternalScripts[jquery]=://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+ExternalScripts[jqueryUI]=://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
 LocalScripts[jquery]=vendor/jquery.min.js
 LocalScripts[jqueryUI]=vendor/jquery-ui.min.js
 
